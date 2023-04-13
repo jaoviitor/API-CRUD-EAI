50564f86d8d38ecde0a7994aa17d2dd295c24987
jaoviitor <joaoviiitorvieira@gmail.com>
2023-04-13 01:53:42 -0300

    add escondendo informações

--- mysql.js
+++ mysql.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
+const dotenv = require('dotenv').config();
 
 var pool = mysql.createPool({
-    "user": "root",
-    "password": "jv5712",
-    "database": "dbEaiConecta",
-    "host": "localhost",
-    "port": 3306
+    "user": process.env.DB_USER,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
+    "port": process.env.DB_PORT
 })
 
 exports.pool = pool;
