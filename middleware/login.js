fd12dc49a803ba6cde730f41a7664d42e76e50ea
jaoviitor <joaoviiitorvieira@gmail.com>
2023-06-08 09:35:19 -0300

    auterações

--- middleware/login.js
+++ middleware/login.js
@@ -2,9 +2,9 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) =>{
     try{
-        const token = req.headers.authorization.split(' ')[1];
-        const decode = jwt.verify(token, process.env.JWT_KEY)
-        req.empresa = decode;
+        
+        const decode = jwt.verify(req.body.token, process.env.JWT_KEY)
+        req.cliente = decode;
         next();
     } catch (error){
         return res.status(401).send({ mensagem: 'Falha na autenticação' })
